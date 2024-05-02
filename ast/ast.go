73c78dddb75e94b73ed140c1b409d9c8689656ea
mountinking <xlavh25576@gmail.com>
2024-05-02 21:50:10 +0900

    -91

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"interpreter-in-go/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -23,32 +27,141 @@ type Program struct {
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
 	}
+	return ""
+}
+
+func (p *Program) String() string {
+	var buf bytes.Buffer
+	for _, s := range p.Statements {
+		buf.WriteString(s.String())
+	}
+	return buf.String()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 type LetStatement struct {
-	Token token.Token // the token.LET token
+	Token token.Token
 	Name  *Identifier
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
-
-type Identifier struct {
-	Token token.Token // the token.IDENT token
-	Value string
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
 }
+func (ls *LetStatement) String() string {
+	var buf bytes.Buffer
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+	buf.WriteString(ls.TokenLiteral() + " ")
+	buf.WriteString(ls.Name.String())
+	buf.WriteString(" = ")
+	if ls.Value != nil {
+		buf.WriteString(ls.Value.String())
+	}
+	buf.WriteString(";")
+	return buf.String()
+}
 
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode()       {}
-func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+func (rs *ReturnStatement) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		buf.WriteString(rs.ReturnValue.String())
+	}
+	buf.WriteString(";")
+	return buf.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString("(")
+	buf.WriteString(pe.Operator)
+	buf.WriteString(pe.Right.String())
+	buf.WriteString(")")
+	return buf.String()
+}
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+func (ie *InfixExpression) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString("(")
+	buf.WriteString(ie.Left.String())
+	buf.WriteString(" " + ie.Operator + " ")
+	buf.WriteString(ie.Right.String())
+	buf.WriteString(")")
+	return buf.String()
+}
